811034b7555464d69d7f3c52dd0e607f7d9d5200
IuliaKovacs <iulia.kovacs@student.upt.ro>
2022-11-08 17:02:39 +0200

    Added login and register user interface

--- 03-frontend/angular-azecommerce/src/app/components/login/login.component.ts
+++ 03-frontend/angular-azecommerce/src/app/components/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
+import { MyCustomValidators } from 'src/app/validators/my-custom-validators';
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginFormGroup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private loginService: LoginService,
+              private router: Router) { 
+      
+    }
 
   ngOnInit(): void {
+    this.loginFormGroup = new FormGroup({
+      emailAddress: new FormControl('', [Validators.required, MyCustomValidators.notOnlyWhitespace, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+      password: new FormControl('', [Validators.required, MyCustomValidators.notOnlyWhitespace, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$&*]).{8,}$')]),
+  })
+  
+  }
+
+  onSubmit(){
+    console.log(this.loginFormGroup.value);
+
+    if (this.loginFormGroup.invalid) {
+      this.loginFormGroup.markAllAsTouched();
+    }
+
   }
 
 }
--- 03-frontend/angular-azecommerce/src/app/components/register/register.component.ts
+++ 03-frontend/angular-azecommerce/src/app/components/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegisterService } from 'src/app/services/register.service';
+import { MyCustomValidators } from 'src/app/validators/my-custom-validators';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerFormGroup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private registerService: RegisterService,
+              private router: Router) { 
+                
+              }
 
   ngOnInit(): void {
+
+    this.registerFormGroup = new FormGroup({
+      emailAddress: new FormControl('', [Validators.required, MyCustomValidators.notOnlyWhitespace, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+      username: new FormControl('', [Validators.required, Validators.minLength(4), MyCustomValidators.notOnlyWhitespace]),
+      password1: new FormControl('', [Validators.required, MyCustomValidators.notOnlyWhitespace, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$&*]).{8,}$')]),
+      password2: new FormControl('', [Validators.required, MyCustomValidators.notOnlyWhitespace, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$&*]).{8,}$')])
+  })
+    
+
+  }
+
+ 
+
+
+
+
+  onSubmit(){
+    console.log(this.registerFormGroup.value);
+
+    if (this.registerFormGroup.invalid) {
+      this.registerFormGroup.markAllAsTouched();
+    }
+
   }
 
 }
